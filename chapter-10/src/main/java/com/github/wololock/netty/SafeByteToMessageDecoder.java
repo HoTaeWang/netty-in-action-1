5b46f06ceaab3c33f09526a863a10c99b4b58807
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-05-22 14:53:32 +0200

    ch10: SafeByteToMessageDecoder added

---
+++ chapter-10/src/main/java/com/github/wololock/netty/SafeByteToMessageDecoder.java
@@ -0,0 +1,23 @@
+package com.github.wololock.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.TooLongFrameException;
+
+import java.util.List;
+
+final class SafeByteToMessageDecoder extends ByteToMessageDecoder {
+
+  private static final int MAX_FRAME_SIZE = 1024;
+
+  @Override
+  protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+    int readable = in.readableBytes();
+    if (readable > MAX_FRAME_SIZE) {
+      in.skipBytes(readable);
+      throw new TooLongFrameException("Frame too big!");
+    }
+    // do something...
+  }
+}
