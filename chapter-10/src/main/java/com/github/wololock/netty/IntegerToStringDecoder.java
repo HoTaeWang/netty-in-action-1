954a66539af0e52e989f58e723d1e86ca9e21c9f
Szymon Stępniak <szymon.stepniak@gmail.com>
2018-05-22 14:50:49 +0200

    ch10: IntegerToStringDecoder added

---
+++ chapter-10/src/main/java/com/github/wololock/netty/IntegerToStringDecoder.java
@@ -0,0 +1,13 @@
+package com.github.wololock.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+final class IntegerToStringDecoder extends MessageToMessageEncoder<Integer> {
+  @Override
+  protected void encode(ChannelHandlerContext ctx, Integer msg, List<Object> out) {
+    out.add(String.valueOf(msg));
+  }
+}
